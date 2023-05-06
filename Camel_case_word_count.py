1ab84c14f640f6cf9e442fedc1a398243381f221
archanajampana <119840726+archanajampana@users.noreply.github.com>
2023-05-06 05:50:47 +0530

    def countWords(str):     count = 1     for i in range(1, len(str) - 1):         if (str[i].isupper()):             count += 1
        return count
    str=input()
    print(countWords(str))

---
+++ Camel_case_word_count.py
@@ -0,0 +1,9 @@
+def countWords(str):
+    count = 1
+    for i in range(1, len(str) - 1):
+        if (str[i].isupper()):
+            count += 1
+ 
+    return count
+str=input()
+print(countWords(str))
