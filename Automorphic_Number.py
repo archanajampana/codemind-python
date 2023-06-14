d0950cea0dc73a5d31fe08ec227a91902f299c59
archanajampana <119840726+archanajampana@users.noreply.github.com>
2023-06-14 18:30:45 +0530

    n=int(input()) s=n*n x=str(n) y=str(s) if y.endswith(x):     print("Automorphic Number") else:     print("Not an Automorphic Number")

--- Automorphic_Number.py
+++ Automorphic_Number.py
@@ -1,8 +1,8 @@
-n = int(input())
-sq = n * n
-s = str(n)
-q = str(sq)
-if q.endswith(s):
+n=int(input())
+s=n*n
+x=str(n)
+y=str(s)
+if y.endswith(x):
     print("Automorphic Number")
 else:
     print("Not an Automorphic Number")
