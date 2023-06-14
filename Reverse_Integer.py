54e2d7a2492c53bc1d8b56bb04916bdc9c09f4ba
archanajampana <119840726+archanajampana@users.noreply.github.com>
2023-06-14 18:49:16 +0530

    n=int(input()) sign=1 rev=0 if n<0:     sign=-1     n=n*-1 while(n>0):     r=n%10     rev=rev*10+r     n=n//10 print("%d"%(sign*rev))

--- Reverse_Integer.py
+++ Reverse_Integer.py
@@ -1,11 +1,12 @@
 n=int(input())
-rev=0
 sign=1
+rev=0
 if n<0:
     sign=-1
-    n=-1*n
-while n>0:
+    n=n*-1
+while(n>0):
     r=n%10
     rev=rev*10+r
     n=n//10
 print("%d"%(sign*rev))
+    
